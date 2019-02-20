4024fbf081245d0c9c212505290f00d21629c1a0
renyi <736290481@qq.com>
2019-02-20 10:46:03 +0800

    本地工厂添加到git

---
+++ FristGit.java
@@ -0,0 +1,9 @@
+package com.itheima;
+
+public class FristGit {
+    public static void main(String[] args) {
+        System.out.println("111111111");
+        System.out.println("111111111");
+        System.out.println("111111111");
+    }
+}
