eef6fa6e01b5809e82677dfb773601aee032c1ff
renyi <736290481@qq.com>
2019-02-20 11:21:00 +0800

    修改

--- git1/FristGit.java
+++ git1/FristGit.java
@@ -1,13 +1,14 @@
-package com.itheima;
-
+package git1;
 public class FristGit {
     public static void main(String[] args) {
         System.out.println("111111111");
         System.out.println("111111111");
         System.out.println("111111111");
+        //qq
     }
     public void test(){
         int a=0;
         a+=10;
+        System.out.println("222");
     }
 }
